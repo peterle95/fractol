21efe188c5af0afdd16a1f3d0f67178e91916c9e
Peter Mölzer <moelzerpeter@gmail.com>
2024-02-21 01:35:45 +0000

    modified headers

--- includes/keys.h
+++ includes/keys.h
@@ -31,9 +31,9 @@
 # define KEY_THREE 51
 # define KEY_FOUR 52
 # define KEY_FIVE 53
-/* # define MOUSE_WHEEL_UP 4
+# define MOUSE_WHEEL_UP 4
 # define MOUSE_WHEEL_DOWN 5
-# define MOUSE_WHEEL_BTN 3
+/*# define MOUSE_WHEEL_BTN 3
 # define MOUSE_BTN 1
 # define MOUSE_BTN_2 3 
 # define KEY_K 107*/
