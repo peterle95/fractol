64ed517a7d9ffd5e26d79e8cc6c986aab275ac25
Peter Moelzer <moelzerpeter@gmail.com>
2024-07-06 21:24:38 +0200

    commit

--- sources/colors_julia.c
+++ sources/colors_julia.c
@@ -12,82 +12,83 @@
 
 #include "../includes/fractol.h"
 
-static int color_julia(t_data *data, double zr, double zi)
+static int	color_julia(t_data *data, double zr, double zi)
 {
-    int iterations;
-    iterations = julia(zr, zi, data->julia_cr, data->julia_ci);
-    return (iterations * 0xFFFFFF / MAX_ITERATIONS);
+	int		iterations;
+
+	iterations = julia(zr, zi, data->julia_cr, data->julia_ci);
+	return (iterations * 0xFFFFFF / MAX_ITERATIONS);
 }
 
 
-void pixel_julia(t_data *data, int x, int y)
+void	pixel_julia(t_data *data, int x, int y)
 {
-    double zr;
-    double zi;
-    int color;
+	double	zr;
+	double	zi;
+	int		color;
 
-    zr = calculate_zr(data, x);
-    zi = calculate_zi(data, y);
-    color = color_julia(data, zr, zi);
-    mlx_pixel_put(data->mlx, data->win, x, y, color);
+	zr = calculate_zr(data, x);
+	zi = calculate_zi(data, y);
+	color = color_julia(data, zr, zi);
+	mlx_pixel_put(data->mlx, data->win, x, y, color);
 }
 
-static int color_julia2(t_data *data, double zr, double zi)
+static int	color_julia2(t_data *data, double zr, double zi)
 {
-    int iterations;
-    iterations = julia(zr, zi, data->julia_cr, data->julia_ci);
-    return (iterations * 0xFFFFFF / MAX_ITERATIONS);
+	int		iterations;
+
+	iterations = julia(zr, zi, data->julia_cr, data->julia_ci);
+	return (iterations * 0xFFFFFF / MAX_ITERATIONS);
 }
 
 
-void pixel_julia2(t_data *data, int x, int y)
+void	pixel_julia2(t_data *data, int x, int y)
 {
-    double zr;
-    double zi;
-    int color;
+	double	zr;
+	double	zi;
+	int		color;
 
-    zr = calculate_zr(data, x);
-    zi = calculate_zi(data, y);
-    color = color_julia2(data, zr, zi);
-    mlx_pixel_put(data->mlx, data->win, x, y, color);
+	zr = calculate_zr(data, x);
+	zi = calculate_zi(data, y);
+	color = color_julia2(data, zr, zi);
+	mlx_pixel_put(data->mlx, data->win, x, y, color);
 }
 
-static int color_julia3(t_data *data, double zr, double zi)
+static int	color_julia3(t_data *data, double zr, double zi)
 {
-    int iterations;
-    iterations = julia(zr, zi, data->julia_cr, data->julia_ci);
-    return (iterations * 0xFFFFFF / MAX_ITERATIONS);
+	int		iterations;
+	iterations = julia(zr, zi, data->julia_cr, data->julia_ci);
+	return (iterations * 0xFFFFFF / MAX_ITERATIONS);
 }
-
 
 void pixel_julia3(t_data *data, int x, int y)
 {
-    double zr;
-    double zi;
-    int color;
+	double	zr;
+	double	zi;
+	int		color;
 
-    zr = calculate_zr(data, x);
-    zi = calculate_zi(data, y);
-    color = color_julia3(data, zr, zi);
-    mlx_pixel_put(data->mlx, data->win, x, y, color);
+	zr = calculate_zr(data, x);
+	zi = calculate_zi(data, y);
+	color = color_julia3(data, zr, zi);
+	mlx_pixel_put(data->mlx, data->win, x, y, color);
 }
 
-static int color_julia4(t_data *data, double zr, double zi)
+static int	color_julia4(t_data *data, double zr, double zi)
 {
-    int iterations;
-    iterations = julia(zr, zi, data->julia_cr, data->julia_ci);
-    return (iterations * 0xFFFFFF / MAX_ITERATIONS);
+	int		iterations;
+	iterations = julia(zr, zi, data->julia_cr, data->julia_ci);
+	return (iterations * 0xFFFFFF / MAX_ITERATIONS);
 }
 
 
-void pixel_julia4(t_data *data, int x, int y)
+void	pixel_julia4(t_data *data, int x, int y)
 {
-    double zr;
-    double zi;
-    int color;
+	double	zr;
+	double	zi;
+	int		color;
 
-    zr = calculate_zr(data, x);
-    zi = calculate_zi(data, y);
-    color = color_julia4(data, zr, zi);
-    mlx_pixel_put(data->mlx, data->win, x, y, color);
+	zr = calculate_zr(data, x);
+	zi = calculate_zi(data, y);
+	color = color_julia4(data, zr, zi);
+	mlx_pixel_put(data->mlx, data->win, x, y, color);
 }
--- sources/colors_mandelbrot.c
+++ sources/colors_mandelbrot.c
@@ -12,82 +12,84 @@
 
 #include "../includes/fractol.h"
 
-static int color_mandelbrot(double cr, double ci)
+static int	color_mandelbrot(double cr, double ci)
 {
-    int iterations;
-    iterations = mandelbrot(cr , ci);
-    return (iterations * 0xFFFFFF / MAX_ITERATIONS);
+	int		iterations;
+
+	iterations = mandelbrot(cr , ci);
+	return (iterations * 0xFFFFFF / MAX_ITERATIONS);
 }
 
 
-void pixel_mandelbrot(t_data *data, int x, int y)
+void	pixel_mandelbrot(t_data *data, int x, int y)
 {
-    double cr;
-    double ci;
-    int color;
+	double	cr;
+	double	ci;
+	int		color;
 
-    cr = calculate_cr(data, x);
-    ci = calculate_ci(data, y);
-    color = color_mandelbrot(cr, ci);
-    mlx_pixel_put(data->mlx, data->win, x, y, color);
+	cr = calculate_cr(data, x);
+	ci = calculate_ci(data, y);
+	color = color_mandelbrot(cr, ci);
+	mlx_pixel_put(data->mlx, data->win, x, y, color);
 }
 
-static int color_mandelbrot2(double cr, double ci)
+static int	color_mandelbrot2(double cr, double ci)
 {
-    int iterations;
-    iterations = mandelbrot(cr , ci);
-    return (iterations * 0xFFFFFF / MAX_ITERATIONS);
-}
+	int		iterations;
 
-
-void pixel_mandelbrot2(t_data *data, int x, int y)
-{
-    double cr;
-    double ci;
-    int color;
-
-    cr = calculate_cr(data, x);
-    ci = calculate_ci(data, y);
-    color = color_mandelbrot2(cr, ci);
-    mlx_pixel_put(data->mlx, data->win, x, y, color);
-}
-
-static int color_mandelbrot3(double cr, double ci)
-{
-    int iterations;
-    iterations = mandelbrot(cr , ci);
-    return (iterations * 0xFFFFFF / MAX_ITERATIONS);
+	iterations = mandelbrot(cr , ci);
+	return (iterations * 0xFFFFFF / MAX_ITERATIONS);
 }
 
 
-void pixel_mandelbrot3(t_data *data, int x, int y)
+void	pixel_mandelbrot2(t_data *data, int x, int y)
 {
-    double cr;
-    double ci;
-    int color;
+	double	cr;
+	double ci;
+	int		color;
 
-    cr = calculate_cr(data, x);
-    ci = calculate_ci(data, y);
-    color = color_mandelbrot3(cr, ci);
-    mlx_pixel_put(data->mlx, data->win, x, y, color);
+	cr = calculate_cr(data, x);
+	ci = calculate_ci(data, y);
+	color = color_mandelbrot2(cr, ci);
+	mlx_pixel_put(data->mlx, data->win, x, y, color);
 }
 
-static int color_mandelbrot4(double cr, double ci)
+static int	color_mandelbrot3(double cr, double ci)
 {
-    int iterations;
-    iterations = mandelbrot(cr , ci);
-    return (iterations * 0xFFFFFF / MAX_ITERATIONS);
+	int		iterations;
+	iterations = mandelbrot(cr , ci);
+	return (iterations * 0xFFFFFF / MAX_ITERATIONS);
 }
 
 
-void pixel_mandelbrot4(t_data *data, int x, int y)
+void	pixel_mandelbrot3(t_data *data, int x, int y)
 {
-    double cr;
-    double ci;
-    int color;
+	double	cr;
+	double	ci;
+	int		color;
 
-    cr = calculate_cr(data, x);
-    ci = calculate_ci(data, y);
-    color = color_mandelbrot4(cr, ci);
-    mlx_pixel_put(data->mlx, data->win, x, y, color);
+	cr = calculate_cr(data, x);
+	ci = calculate_ci(data, y);
+	color = color_mandelbrot3(cr, ci);
+	mlx_pixel_put(data->mlx, data->win, x, y, color);
+}
+
+static int	color_mandelbrot4(double cr, double ci)
+{
+	int		iterations;
+	iterations = mandelbrot(cr , ci);
+	return (iterations * 0xFFFFFF / MAX_ITERATIONS);
+}
+
+
+void	pixel_mandelbrot4(t_data *data, int x, int y)
+{
+	double	cr;
+	double	ci;
+	int		color;
+
+	cr = calculate_cr(data, x);
+	ci = calculate_ci(data, y);
+	color = color_mandelbrot4(cr, ci);
+	mlx_pixel_put(data->mlx, data->win, x, y, color);
 }
