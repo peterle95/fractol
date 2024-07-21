18395f4eca229aeb8ea48140a63390fa69de9bbf
Peter Moelzer <moelzerpeter@gmail.com>
2024-07-21 13:35:10 +0200

    commit

---
+++ sources/utils2.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils2.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pmolzer <pmolzer@student.42berlin.de>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/21 12:37:22 by pmolzer           #+#    #+#             */
+/*   Updated: 2024/07/21 13:12:40 by pmolzer          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/fractol.h"
+
+int validate_julia(char *str)
+{
+    int i = 0;
+    int digits_after_point = 0;
+
+    // Check for optional negative sign
+    while (str[i] == '-' || str[i] == '+')
+        i++;
+
+    // There should be one digit before decimal point
+    if (!ft_isdigit(str[i]))
+        return (1);
+    i++;
+
+    // Check for decimal point
+    if (str[i] != '.')
+        return (1);
+    i++;
+
+    // Check for one to three digits after decimal point
+    while (str[i])
+    {
+        if (!ft_isdigit(str[i]))
+            return (1);
+        digits_after_point++;
+        i++;
+    }
+
+    if (digits_after_point < 1 || digits_after_point > 3)
+        return (1);
+
+    return (0);
+}
