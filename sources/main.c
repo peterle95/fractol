84103f3ec6d41e2f987d3cef59fea3ad78c8efab
Peter Moelzer <moelzerpeter@gmail.com>
2024-07-21 13:37:55 +0200

    commit

--- sources/main.c
+++ sources/main.c
@@ -6,27 +6,11 @@
 /*   By: pmolzer <pmolzer@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/02 13:14:30 by pmolzer           #+#    #+#             */
-/*   Updated: 2024/07/07 18:03:06 by pmolzer          ###   ########.fr       */
+/*   Updated: 2024/07/21 13:37:42 by pmolzer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/fractol.h"
-
-/*We create a separate parse_args function to handle the argument parsing. This keeps the main function clean and makes the code more modular.
-We check if there's at least one argument (the program name is argc[0], so we need argc >= 2).
-We use strcmp to check if the first argument is "mandelbrot" or "julia". This is simple and straightforward.
-For the Mandelbrot set, we don't need additional parameters, so we just set the fractal type.
-We check if exactly 3 arguments are provided (program name, "julia", and two parameters).
-We validate each parameter using is_valid_float.
-We convert the parameters to doubles using atof.
-We check if the parameters are within the range -2 to 2, which is a common range for interesting Julia sets.
-
-We return 0 from parse_args if:
-
-The fractal type is not recognized.
-The Julia set is selected but the number of arguments is incorrect.
-The Julia set parameters are not valid floats.
-The Julia set parameters are out of the acceptable range.*/
 
 void	draw_fractal(t_data *data)
 {
@@ -48,21 +32,24 @@ void	draw_fractal(t_data *data)
 		draw_julia4(data);
 }
 
-int	julia_checker(t_data *data, char argc, char **argv)
+int julia_checker(t_data *data, int argc, char **argv)
 {
-	data->fractal_type = JULIA;
-	if (argc == 4)
-	{
-		data->julia_ci = convert_float(argv[2]);
-		data->julia_cr = convert_float(argv[3]);
-		if (data->julia_ci == -42.0 || data->julia_cr == -42.0
-			|| data->julia_ci < -2.0 || data->julia_ci > 2.0
-			|| data->julia_cr < -2.0 || data->julia_cr > 2.0)
-			return (0);
-	}
-	else if (argc != 2)
-		return (0);
-	return (0);
+    data->fractal_type = JULIA;
+    if (argc == 4)
+    {
+        if (validate_julia(argv[2]) || validate_julia(argv[3]))
+                return (1);
+        data->julia_ci = convert_float(argv[2]);
+        data->julia_cr = convert_float(argv[3]);
+        if (data->julia_ci == -42.0 || data->julia_cr == -42.0)
+            return (1);
+        if (data->julia_ci < -2.0 || data->julia_ci > 2.0 || 
+            data->julia_cr < -2.0 || data->julia_cr > 2.0)
+                    return (1);
+                return (0);
+    }
+    else
+        return (1);
 }
 
 int	parse_args(int argc, char **argv, t_data *data)
@@ -97,7 +84,6 @@ int	main(int argc, char **argv)
 	}
 	if (!parse_args(argc, argv, &data))
 	{
-		printf("Error: Invalid arguments\n");
 		print_usage();
 		exit_program(&data);
 		return (1);
