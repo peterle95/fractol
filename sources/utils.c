cbdd5a8dae7bc81505ba47cddf61a5b20cbd5a2c
Peter Moelzer <moelzerpeter@gmail.com>
2024-07-21 13:39:17 +0200

    commit

--- sources/utils.c
+++ sources/utils.c
@@ -6,7 +6,7 @@
 /*   By: pmolzer <pmolzer@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/02 14:31:37 by pmolzer           #+#    #+#             */
-/*   Updated: 2024/07/05 11:59:56 by pmolzer          ###   ########.fr       */
+/*   Updated: 2024/07/21 13:39:05 by pmolzer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,10 @@
 
 void	print_usage(void)
 {
+	printf("Error: Invalid arguments\n");
+	printf("Julia parameters must be in the format x.x, x.xx, or x.xxx\n");
 	printf("Julia set parameters must be between -2.0 and 2.0\n");
+	printf("Maldelbrot does not need any parameters\n");
 	printf("Usage: [mandelbrot | julia <real value> <imaginary value>]\n");
 	printf("Example: ./fractol julia 0.1 -0.7\n");
 	printf("\t ./fractol mandelbrot\n");
@@ -63,14 +66,14 @@ double	convert_float(char *str)
 	div = 0.1;
 	is_neg = 1;
 	i = skip_space_and_sign(str, &is_neg);
-	while (str[i] && ft_isdigit(str[i]) && str[i] != '.')
+	while (str[i] && str[i] != '.')
 	{
 		nb = (nb * 10.0) + (str[i] - '0');
 		i++;
 	}
 	if (str[i] == '.')
 		i++;
-	while (str[i] && ft_isdigit(str[i]))
+	while (str[i])
 	{
 		nb = nb + ((str[i] - '0') * div);
 		div *= 0.1;
